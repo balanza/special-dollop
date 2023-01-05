7e2cde43dba8c870061f71a61b8bee115ed3a5dd
Emanuele De Cupis <ema.decu@gmail.com>
2023-01-05 15:05:23 +0100

    refactor: use tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,7 @@
+
+
+describe("simple test scenario", () => {
+    it("simple test case", ()=>{
+        expect(1).toBe(1)
+    })
+});
---
+++ __tests__/scenario1.test.js
@@ -0,0 +1,5 @@
+describe('Scenario 1', () => {
+    it('should ', () => {
+        
+    });
+});
